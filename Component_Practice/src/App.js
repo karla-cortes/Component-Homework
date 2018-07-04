8903c631b4755547744be603f9c1a7a3194803db
Karla Cortes <kmcortes7@gmail.com>
2018-07-04 13:59:56 -0400

    Files

--- Component_Practice/src/App.js
+++ Component_Practice/src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Main from './components/Main';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import SocialLinks from './components/SocialLinks';
+import Project from './components/Project';
+import Footer from './components/Footer';
 import './App.css';
 
 class App extends Component {
@@ -10,6 +18,7 @@ class App extends Component {
         <Home />
         <Main />
         <Projects />
+        <Project />
         <Resume />
         <Contact />
         <SocialLinks />
--- Component_Practice/src/components/Projects.js
+++ Component_Practice/src/components/Projects.js
@@ -1,1 +1,14 @@
+import React from 'react';
 import projects from '../data/projects.json';
+
+class Projects extends React.Component {
+     render() {
+       return (
+         <div>
+         <projects />
+         </div>
+       )
+   }
+}
+
+export default Projects;
